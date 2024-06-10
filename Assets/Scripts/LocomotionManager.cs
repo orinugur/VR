688595516d1c2ea6d87b06895ec774373772871a
조현승 <danitech171791@gmail.com>
2024-06-10 16:44:25 +0900

    [feat] Locomotion Manager - 필요 컴포넌트 참조부 추가
    - XRBaseController : VR/AR 환경에서 컨트롤러 입력을 처리하기 위한 기본 컴포넌트
    - ContinuousMoveProviderBase : 연속적인 이동을 구현 / 연속 이동을 위한 입력을 처리
    - ContinuousTurnProviderBase : 사용자가 자연스럽게 회전할 수 있도록 함 / 연속적인 회전을 구현
    - SnapTurnProviderBase : 방향의 순간적인 변경 가능하게 함 / 멀미 최소화 유용
    - TeleportationProvider : 사용자가 순간 이동 할 수 있도록 하는 컴포넌트, 장애물을 피하거나 먼 거리를 이동할 때 유용

--- Assets/Scripts/LocomotionManager.cs
+++ Assets/Scripts/LocomotionManager.cs
@@ -7,4 +7,14 @@ public class LocomotionManager : MonoBehaviour
     public enum MoveStyleType { HeadRelative, HandRelative }
     public enum TurnStyleType { Snap, Continuous }
 
+    [Header("XR")]
+    [SerializeField] XROrigin XROrigin;
+    [SerializeField] XRBaseController Controller_Left;
+    [SerializeField] XRBaseController Controller_Right;
+
+    [Header("Locomotion")]
+    [SerializeField] ContinuousMoveProviderBase Provider_ContinuousMove;
+    [SerializeField] ContinuousTurnProviderBase Provider_ContinuousTurn;
+    [SerializeField] SnapTurnProviderBase Provider_SnapTurn;
+    [SerializeField] TeleportationProvider Provider_Teleportation;
 }
